acfffea212c30a713214f9ec6fdcb977afea50f9
saranya-1621 <51949644+saranya-1621@users.noreply.github.com>
2019-07-06 12:48:35 +0530

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,6 +1,5 @@
-n1 = int(input())
-num1 = input().split()
-for i in num1:
-    if num1.count(i) == 1:
-        print(i)
-        break
+N2=int(input())
+n1=list(map(int,input().split()))
+for k1 in range(len(n1)):
+    if n1.count(n1[k1])==1:
+        print(n1[k1])
