a15fdba47471c35b4c64b6995dd6d1acd2936ef6
saranya-1621 <51949644+saranya-1621@users.noreply.github.com>
2019-07-12 14:44:22 +0530

    Update 2sp.py

--- 2sp.py
+++ 2sp.py
@@ -1,11 +1,11 @@
-cho2,sawa2=map(int,input().split())
-lost2=list(map(int,input().split()))
-law2=[]
-for p in range(0,sawa2):
-     sha2,she2=map(int,input().split())
-     law2.append([sha2,she2])
-for p in range(sawa2):
-     las2=law2[p][0]
-     upo2=law2[p][1]
-     yaw2=sum(lost2[last2-1:upo2])
-     print(yaw2)
+ka,ma=map(int,input().split())
+mka=list(map(int,input().split()))
+l11=[]
+for i in range(0,ma):
+     aa,ba=map(int,input().split())
+     l11.append([aa,ba])
+for i in range(ma):
+     lower=l11[i][0]
+     upper=l11[i][1]
+     xa=sum(mka[lower-1:upper])
+     print(xa)
