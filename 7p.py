65a4de70c3b903b531ddbe3dee7e4781688e0718
saranya-1621 <51949644+saranya-1621@users.noreply.github.com>
2019-07-12 09:16:41 +0530

    Update 7p.py

--- 7p.py
+++ 7p.py
@@ -1,7 +1,8 @@
-nopo=int(input())
-gero=[]
-powo=0
-for i in range (0,nopo+1):
-    pow=abso((2**i)-nopo)
-    ger.append(powo)
-print(min(gero))
+km122=int(input())
+km212=[]
+guvi=0
+for h in range (0,km122+1):
+    guvi=abs((2**h)-km122)
+    km212.append(guvi)
+stn=min(km212)
+print(stn)
